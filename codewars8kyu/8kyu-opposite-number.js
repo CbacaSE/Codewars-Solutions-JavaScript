65a28f1cd85a779c366d7b309c034995826be36c
Chantal Baca <chantalbaca3@gmail.com>
2022-05-26 13:16:39 -0500

    Daily Katas

---
+++ codewars8kyu/8kyu-opposite-number.js
@@ -0,0 +1,24 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+const opposite = number => -number
+
+/* TEST CASES:
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(opposite(1), -1,)
+Test.assertEquals(opposite(0), 0,)
+Test.assertEquals(opposite(4.25), -4.25,)
+Test.assertEquals(opposite(3.3333333), -3.3333333,)
+Test.assertEquals(opposite(-12525220.3325), 12525220.3325,)
+Test.assertEquals(opposite(-5), 5,)
+  });
+});
+*/
