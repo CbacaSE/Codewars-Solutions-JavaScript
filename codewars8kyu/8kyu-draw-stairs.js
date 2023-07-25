88d7f544e33b57b48da91f95ca6c9ff951aae309
Chantal Baca <chantalbaca3@gmail.com>
2023-07-25 18:59:24 -0500

    Daily Katas

--- codewars8kyu/8kyu-draw-stairs.js
+++ codewars8kyu/8kyu-draw-stairs.js
@@ -22,4 +22,14 @@
 //      I
 //       I
 
-
+function drawStairs(n) {
+    if (n > 1){ 
+    let str = ''
+    for (let i = 1; i <n; i++){
+        str += (`I\n${' '.repeat(i)}`)
+    }
+        return str + 'I';
+    } else {
+        return 'I';
+    }
+}
