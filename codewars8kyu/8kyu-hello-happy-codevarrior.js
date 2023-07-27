c53c356208979f8a63450af79b4e6273cde44f28
Chantal Baca <chantalbaca3@gmail.com>
2023-07-27 15:51:48 -0500

    Daily Katas

--- codewars8kyu/8kyu-hello-happy-codevarrior.js
+++ codewars8kyu/8kyu-hello-happy-codevarrior.js
@@ -1,5 +1,17 @@
+// Description:
+// VVhat ?!?
+
+// None of zese codevarriors seemz to remember hiz ovvn name !
+
+// Kould you help ?
+
+// var albert = new Warrior("Al")
+// var boris  = new Warrior("Boris")
+
+// albert.toString() --> "Hi! my name's Boris" <-- ohoh..
 
 
+// Solution:
 
 function Warrior(n){
     var name = n;  
