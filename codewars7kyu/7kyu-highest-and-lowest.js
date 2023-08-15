e41814ca9184eefae47a0e0463128697990255a4
Chantal Baca <chantalbaca3@gmail.com>
2023-08-15 16:20:40 -0500

    Daily Katas

--- codewars7kyu/7kyu-highest-and-lowest.js
+++ codewars7kyu/7kyu-highest-and-lowest.js
@@ -16,5 +16,9 @@
 //Solution:
 
 function highAndLow(numbers){
+    const numArray = numbers.split(' ').map(Number);
+    const max = Math.max(...numArray);
+    const min = Math.min(...numArray);
     
+    return `${max} ${min}`;
 }
