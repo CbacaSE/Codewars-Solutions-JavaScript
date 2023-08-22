c6f8a471f033a0dc814a22e27f6e05f52af0cdff
Chantal Baca <chantalbaca3@gmail.com>
2023-08-22 16:01:19 -0500

    Daily Katas

--- codewars8kyu/8kyu-sum-arrays.js
+++ codewars8kyu/8kyu-sum-arrays.js
@@ -27,7 +27,11 @@
 
 // Sum Numbers
 function sum (numbers) {
-    "use strict";
-    
-    
-};
+    //     "use strict";
+        let sum = 0;
+        for(let i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+        
+    };;
