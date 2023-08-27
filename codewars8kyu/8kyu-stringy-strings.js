9e89748751ce9c342aef2cc6698b1cabbb6d53c9
Chantal Baca <chantalbaca3@gmail.com>
2023-08-27 13:30:09 -0500

    Daily Katas

--- codewars8kyu/8kyu-stringy-strings.js
+++ codewars8kyu/8kyu-stringy-strings.js
@@ -11,3 +11,12 @@
 // with size 12 should return : '101010101010'.
 
 // The size will always be positive and will only use whole numbers.
+
+
+function stringy(size) {
+    let result = '';
+    for(let i = 0; i < size; i++) {
+        result += i % 2 === 0 ? '1' : '0';
+    }
+    return result;
+}
