307cfb26794c573c325e58f6e34e8a27078fe9e5
Chantal Baca <chantalbaca3@gmail.com>
2023-09-07 17:04:31 -0500

    Daily Katas

--- codewars7kyu/7kyu-vowel-count.js
+++ codewars7kyu/7kyu-vowel-count.js
@@ -8,3 +8,17 @@
 
 
 //Solution:
+
+function getCount(str) {
+    const vowels = 'aeiou';
+    let count = 0;
+    
+    for(let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            count++;
+        }
+    }
+    
+    return count;
+};
+
